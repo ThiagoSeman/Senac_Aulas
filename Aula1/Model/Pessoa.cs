0858fd49c96352dfb354e54090a8d96d3ab96698
Thiago Seman <thiago@sisgel.com>
2023-08-01 07:56:43 -0300

    Adicionar arquivos de projeto.

---
+++ Aula1/Model/Pessoa.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula1.Model
+{
+    public class Pessoa
+    {
+        public int id { get; set; }
+        public string nome { get; set; }
+        public DateTime DataNac { get; set; }
+        public string CPF { get; set; }
+        public Endereco endereco { get; set; }
+    }
+
+    public class Endereco
+    {
+        public int id { get; set; }
+        public string logradouro { get; set; }
+        public string numero { get; set; }
+    }
+
+
+
+}
---
+++ Aula1/Program.cs
@@ -0,0 +1,28 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Aula1.Model;
+using System;
+
+namespace Aula1 // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        public static Pessoa Cliente { get; set; }=new Pessoa();
+
+        static void Main(string[] args)
+        {
+            Cliente.id = 1;
+            Cliente.nome = "Gilberto";
+            Cliente.DataNac = (new DateTime(31 / 08 / 2023));
+            Cliente.CPF = "222.222.222-22";
+            Cliente.endereco = new Endereco();
+            Cliente.endereco.id = 1;
+            Cliente.endereco.logradouro = "Rua Brasil";
+            Cliente.endereco.numero = "ao lado do 500";
+            
+        Console.WriteLine($"ID = {Cliente.id}");
+        }
+    }
+}
+
+
